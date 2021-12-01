ff4c9ced66178580b4d6e404d01c15f34069def6
Ruben Deisenroth <rdeisenroth@gmail.com>
2021-12-01 18:14:06 +0100

    --no-edit

--- solution/src/main/java/h05/FamilyFriendlyZoo.java
+++ solution/src/main/java/h05/FamilyFriendlyZoo.java
@@ -12,14 +12,27 @@ public class FamilyFriendlyZoo implements Zoo {
 
     @Override
     public boolean canLiveTogether(Animal a1, Animal a2) {
+        return canLiveTogetherHelper(a1, a2, false);
+    }
+
+    /**
+     * Returns true if two {@link Animal}s are Compatible
+     *
+     * @param a1      the first {@link Animal}
+     * @param a2      the second {@link Animal}
+     * @param swapped whether the animals have already been swapped (call with
+     *                false)
+     * @return true if two {@link Animal}s are Compatible
+     */
+    private boolean canLiveTogetherHelper(Animal a1, Animal a2, boolean swapped) {
         // Haie sind nicht mit anderen Knorpelfischen kompatibel
         if (a1 instanceof Shark && a2.getAnimalType() == AnimalType.CHONDRICHTHYES && !(a2 instanceof Shark)) {
-            return false;
+            return swapped ? false : canLiveTogetherHelper(a2, a1, true);
         }
 
         // Krokodile dürfen nicht mit Säugetieren zusammenleben
         if (a1.getAnimalType() == AnimalType.CROCODYLIDAE && a2.getAnimalType() == AnimalType.MAMMALIA) {
-            return false;
+            return swapped ? false : canLiveTogetherHelper(a2, a1, true);
         }
 
         // Vögel dürfen nur mit Tieren zusammenleben, die (falls Sie laufen können) bei
@@ -28,7 +41,7 @@ public class FamilyFriendlyZoo implements Zoo {
                 && ((Walking) a2).getAverageSpeed(10) >= 3) {
             // Haiart 4 darf nicht mit anderen Haiarten zusammenleben
             // Krokodile Sind nur mit
-            return false;
+            return swapped ? false : canLiveTogetherHelper(a2, a1, true);
         }
         return true;
     }
