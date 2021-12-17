b22298113a2f70fa0f440d4bb850e9f8b6aa1cc7
Ruben Deisenroth <rdeisenroth@gmail.com>
2021-12-17 18:37:53 +0100

    Allow more correct variants of clone

--- solution/build.gradle.kts
+++ solution/build.gradle.kts
@@ -17,6 +17,5 @@ dependencies {
   testImplementation("org.mockito:mockito-core:4.1.0")
   testImplementation("com.google.guava:guava:31.0.1-jre")
   testImplementation("org.sourcegrade:docwatcher-api:0.1.0-SNAPSHOT")
-  testImplementation("org.sourcegrade:docwatcher-core:0.1.0-SNAPSHOT")
   testImplementation("fr.inria.gforge.spoon:spoon-core:10.0.0")
 }
--- solution/src/test/java/h05/TutorTests_H3_4.java
+++ solution/src/test/java/h05/TutorTests_H3_4.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -46,27 +47,10 @@ public class TutorTests_H3_4 {
     public void t02() {
         saltWaterCrocodileAsAmphibeanCT.resolveReal();
         var mt = new MethodTester(
-            saltWaterCrocodileAsAmphibeanCT, "clone", 0.8, Modifier.PUBLIC, Object.class, null, false, true).verify();
-        Field saltyField = saltWaterCrocodileAsAmphibeanCT
-            .resolveAttribute(new AttributeMatcher("salty", 0.8, -1, short.class));
-
-        var saltyInstance = saltWaterCrocodileCT.resolve().getClassInstance();
-
-        saltWaterCrocodileAsAmphibeanCT.setField(saltyField, saltyInstance);
-
-        var clonedSWCAA = mt.invoke();
-        assertNotSame(saltyInstance, clonedSWCAA);
-        assertNotNull(clonedSWCAA, "Falsche Rückgabe bei Methode Clone: ");
-        assertSame(saltyInstance, ClassTester.getFieldValue(clonedSWCAA, saltyField));
-    }
-
-    @Test
-    @DisplayName("2-Alt | Methode Clone")
-    public void t02_alt2() {
-        saltWaterCrocodileAsAmphibeanCT.resolveReal();
-        var mt = new MethodTester(
-            saltWaterCrocodileAsAmphibeanCT, "clone", 0.8, Modifier.PUBLIC | Modifier.VOLATILE, Object.class, null,
-            false, true).verify();
+            saltWaterCrocodileAsAmphibeanCT, "clone", 0.8, -1, Object.class, null, false, true).verify();
+        // Hier gab es mehrere korrekte Möglichkeiten, wodurch der Modifier nicht exakt
+        // vorgegeben war. Public muss es aber sein
+        assertTrue(Modifier.isPublic(mt.getAccessModifier()), "Methode Clone ist nicht public.");
         Field saltyField = saltWaterCrocodileAsAmphibeanCT
             .resolveAttribute(new AttributeMatcher("salty", 0.8, -1, short.class));
 
