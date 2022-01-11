510da29089e30ebc7c27f0c0567732b99e39a2b1
Alexander Staeding <alexanderstaeding@gmail.com>
2022-01-11 16:03:00 +0100

    Remove redundant test dependencies in solution

--- solution/build.gradle.kts
+++ solution/build.gradle.kts
@@ -10,12 +10,3 @@ submit {
     firstName = "sol_first"
     lastName = "sol_last"
 }
-
-dependencies {
-    testImplementation("org.sourcegrade:jagr-grader-api:0.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("org.mockito:mockito-core:4.1.0")
-    testImplementation("com.google.guava:guava:31.0.1-jre")
-    testImplementation("org.sourcegrade:docwatcher-api:0.1-SNAPSHOT")
-//   testImplementation("fr.inria.gforge.spoon:spoon-core:10.0.0")
-}
